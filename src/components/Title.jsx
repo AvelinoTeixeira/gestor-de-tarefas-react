34d5bc810d6ae27abc74608206f79ba2a290efaf
Avelino Teixeira <137221452+AvelinoTeixeira@users.noreply.github.com>
2025-08-06 16:41:26 +0200

    Correção do title

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -1,6 +1,6 @@
 function Title({children}) {
     return(
-        <h1 className="text-3xl text-slate-100 font-bold text-center">
+        <h1 className="text-3xl text-slate-400 font-bold text-center">
             {children}
         </h1>
     )
